fcc642c32b3faa0855d8d3fbd511d1f944032d4d
User <user@example.com>
2025-12-11 20:53:19 +0800

    Prepare Vite config for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
